0b1396e95cbe8d79cdeacc60a8fa30cbe68b60c4
Fidalgo <pedrofidalgo7@gmail.com>
2019-03-24 16:40:35 +0000

    build test

---
+++ build/precache-manifest.2457318a77102a0fbdae19673c58012b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "90c2c7e3563a15b7c07f",
+    "url": "/static/js/main.580b7405.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89ccaa05c186da9b9c4a",
+    "url": "/static/css/2.4ae23ddc.chunk.css"
+  },
+  {
+    "revision": "89ccaa05c186da9b9c4a",
+    "url": "/static/js/2.9faa26c0.chunk.js"
+  },
+  {
+    "revision": "4c58037b84e2b796d404d3cb2770a210",
+    "url": "/static/media/nothing.4c58037b.svg"
+  },
+  {
+    "revision": "fdbc4613a1ea54685aefe7e130011d3c",
+    "url": "/static/media/person.fdbc4613.svg"
+  },
+  {
+    "revision": "bcc287eaf67b81a8f6332e2dff80eb16",
+    "url": "/static/media/logo.bcc287ea.svg"
+  },
+  {
+    "revision": "e0b5f43f549eaaae96de6476830468fa",
+    "url": "/static/media/empty.e0b5f43f.svg"
+  },
+  {
+    "revision": "cbc8724a992b15ec4f477b412ec14120",
+    "url": "/static/media/error.cbc8724a.svg"
+  },
+  {
+    "revision": "9ab4a7fed8f0282e1a14004178cf6a18",
+    "url": "/index.html"
+  }
+];
