8b00c8ee2dde506b45db1a6ab907719dd95eb3a6
richard <inf.darzegmail.com>
2019-03-26 02:55:11 -0500

    add breakpoints

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -7,23 +7,20 @@ const theme = {
     text: '#fafafa',
     link: '#444444',
   },
-  size: {
-    smallest: '25em', //275px
-    smaller: '31.25em', //500px
-    small: '37.5em', //600px
-    medium: '56.25em', //900px
-    large: '80em', //1300px
-    larger: '90em', //1300px
-    largest: '97em', //1500px
-  },
+  breakpoints: [
+    '56.25em',
+    '80em',
+    '90em',
+    '97em',
+  ],
   mediaQueries: {
-    smallest: `only screen and (max-width: 25em)`,
-    smaller: 'only screen and (max-width: 31.25em)',
-    small: 'only screen and (max-width: 37.5em)',
-    medium: 'only screen and (max-width: 56.25em)',
-    large: 'only screen and (max-width: 80em)',
-    larger: 'only screen and (max-width: 90em)',
-    largest: 'only screen and (max-width: 97em)',
+    smallest: `only screen and (max-width: 25em)`, //275px
+    smaller: 'only screen and (max-width: 31.25em)', //500px
+    small: 'only screen and (max-width: 37.5em)', //600px
+    medium: 'only screen and (max-width: 56.25em)', //900px
+    large: 'only screen and (max-width: 80em)', //1300px
+    larger: 'only screen and (max-width: 90em)', //1300px
+    largest: 'only screen and (max-width: 97em)', //1500px
   },
 };
 
