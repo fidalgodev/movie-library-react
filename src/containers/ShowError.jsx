2181f00e1e67f687867761d3dc25800f0df198ee
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2026-04-11 17:23:04 +0100

    Rewrite ShowError with hooks, clear-before-navigate pattern

--- src/containers/ShowError.jsx
+++ src/containers/ShowError.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import history from '../history';
 import { animateScroll as scroll } from 'react-scroll';
 
-import { clearError } from '../actions';
+import { clearError } from '../slices/errorsSlice';
 import ErrorSvg from '../svg/error.svg';
 import Button from '../components/Button';
 
@@ -18,7 +17,7 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: space-evenly;
 
-  @media ${props => props.theme.mediaQueries.medium} {
+  @media ${(props) => props.theme.mediaQueries.medium} {
     width: 65%;
   }
 `;
@@ -43,28 +42,25 @@ const SubTitle = styled.h2`
   font-size: 1.8rem;
 `;
 
-const LinkWrapper = styled(Link)`
-  text-decoration: none;
-`;
-
 const Svg = styled.img`
   max-width: 100%;
   height: 35vh;
   margin-bottom: 6rem;
 `;
 
-const ShowError = ({ errors, clearError }) => {
-  useEffect(() => {
-    scroll.scrollToTop({
-      smooth: true,
-    });
-    return () => clearError();
-  }, []);
+const ShowError = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const error = useSelector((state) => state.errors);
 
-  if (errors.length === 0) {
-    history.push(`${process.env.PUBLIC_URL}/`);
-    return null;
-  }
+  // Dispatch clearError BEFORE navigating so RouteGuard sees null error
+  // and doesn't redirect back to /error in a loop.
+  const handleHomeClick = useCallback(() => {
+    dispatch(clearError());
+    scroll.scrollToTop({ smooth: true });
+    navigate('/');
+  }, [dispatch, navigate]);
+
   return (
     <Wrapper>
       <Helmet>
@@ -72,19 +68,14 @@ const ShowError = ({ errors, clearError }) => {
       </Helmet>
       <TitleWrapper>
         <Title>Something went wrong!</Title>
-        <SubTitle>{errors.data.status_message}</SubTitle>
+        <SubTitle>{error?.message ?? 'Something went wrong'}</SubTitle>
       </TitleWrapper>
       <Svg src={`${ErrorSvg}`} alt="Not found" />
-      <LinkWrapper to={`${process.env.PUBLIC_URL}/`}>
+      <div onClick={handleHomeClick}>
         <Button title="Home" solid icon="home" left />
-      </LinkWrapper>
+      </div>
     </Wrapper>
   );
 };
 
-const mapStateToProps = ({ errors }) => ({ errors });
-
-export default connect(
-  mapStateToProps,
-  { clearError }
-)(ShowError);
+export default ShowError;
