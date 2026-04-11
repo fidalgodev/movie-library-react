0f7cbc97c383fa92e7eb2fe82db21956843e2191
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2026-04-11 17:06:06 +0100

    Replace src/index.js and containers/App.js with src/main.jsx and src/App.jsx
    React 19 createRoot, BrowserRouter, HelmetProvider, moved top-level chrome
    out of containers/.

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { store } from './store';
+import theme from './utils/theme';
+import GlobalStyle from './utils/globals';
+import App from './App';
+
+import 'react-modal-video/scss/modal-video.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <HelmetProvider>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Helmet>
+              <title>Movie Library</title>
+              <meta
+                name="description"
+                content="A Movie Library where you can check all your favorite movies, as well as the cast of it, and so much more! Made with ❤️ by Fidalgo"
+              />
+              <link rel="canonical" href="https://movies.fidalgo.dev" />
+            </Helmet>
+            <GlobalStyle />
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
+    </Provider>
+  </StrictMode>
+);
