c65e683d0e636de509b21e52b0f971acab2fc91b
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2026-04-11 16:59:12 +0100

    Implement movieSlice with split fetchMovie and fetchCredits thunks

---
+++ src/slices/movieSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import tmdbAPI from '../api/tmdb';
+
+export const fetchMovie = createAsyncThunk(
+  'movie/fetchMovie',
+  async (id) => {
+    const res = await tmdbAPI.get(`/movie/${id}`, {
+      params: { append_to_response: 'videos' },
+    });
+    return res.data;
+  }
+);
+
+export const fetchCredits = createAsyncThunk(
+  'movie/fetchCredits',
+  async (id) => {
+    const res = await tmdbAPI.get(`/movie/${id}/credits`);
+    return res.data.cast;
+  }
+);
+
+const movieSlice = createSlice({
+  name: 'movie',
+  initialState: {
+    data: null,
+    cast: [],
+    loading: true,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovie.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchMovie.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchCredits.fulfilled, (state, action) => {
+        state.cast = action.payload;
+      });
+  },
+});
+
+export default movieSlice.reducer;
